d50105649ea8b6ea9977db77b4bdf0978cc7a765
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-10-16 16:33:21 -0700

    Position cavas/stage

--- Scripts/stage.js
+++ Scripts/stage.js
@@ -1,9 +1,20 @@
 
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var renderer = PIXI.autoDetectRenderer(512, 512, {
+	transparent: true,
+	resolution: 1,
+	antialias: true
+});
 
 document.getElementById('ChartStage').appendChild(renderer.view);
-// console.log(document.getElementById('ChartStage'))
 
 var stage = new PIXI.Container();
+
+const dot = new PIXI.Graphics();
+dot.beginFill(000);
+dot.drawCircle(0, 0, 10);
+dot.x = 100;
+dot.y = 100;
+stage.addChild(dot);
+
 
 renderer.render(stage);
