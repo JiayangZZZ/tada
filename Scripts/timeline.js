7acd9f14940bc389f09e6c11e95f4c43bbd5a602
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-10-20 17:24:39 -0700

    WIP Add timeline

---
+++ Scripts/timeline.js
@@ -0,0 +1,17 @@
+var width = 1000
+	, height =550;
+
+// var renderer2 = PIXI.autoDetectRenderer(width, height, {
+// 	transparent: true,
+// 	resolution: 1,
+// 	antialias: true
+// });
+
+// document.getElementById('Footer').appendChild(renderer2.view);
+// var stage = new PIXI.Container();
+
+// function onDragStart(event) {
+// 	this.data = event.data;
+// 	this.alpha = 0.5;
+// 	this.dragging = true;
+// }
