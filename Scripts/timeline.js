090fc55abda7b11452770b36f8c0f3aa7dd5f3c6
Jiayang <jiayangzhang1104@gmail.com>
2017-10-22 11:57:24 -0700

    WIP Make footer timeline slider draggable

--- Scripts/timeline.js
+++ Scripts/timeline.js
@@ -10,8 +10,36 @@ var width = 1000
 // document.getElementById('Footer').appendChild(renderer2.view);
 // var stage = new PIXI.Container();
 
-// function onDragStart(event) {
-// 	this.data = event.data;
-// 	this.alpha = 0.5;
-// 	this.dragging = true;
-// }
+
+var lowerHandle = $('.TimelineHandleLower')
+	, upperHandle = $('.TimelineHandleUpper');
+
+addListeners();
+
+function addListeners(){
+	lowerHandle.on('mousedown', lowerHandleMouseDown);
+	upperHandle.on('mousedown', upperHandleMouseDown);
+	window.addEventListener('mouseup', mouseUp, false);
+}
+
+function mouseUp()
+{
+	window.removeEventListener('mousemove', lowerHandleMove, true);
+	window.removeEventListener('mousemove', upperHandleMove, true);
+}
+
+function lowerHandleMouseDown(e){
+	window.addEventListener('mousemove', lowerHandleMove, true);
+}
+
+function lowerHandleMove(e){
+	lowerHandle.css({left: e.clientX + 'px'});
+}
+
+function upperHandleMouseDown(e){
+	window.addEventListener('mousemove', upperHandleMove, true);
+}
+
+function upperHandleMove(e){
+	upperHandle.css({left: e.clientX + 'px'});
+}
